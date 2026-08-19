ccaf7cdfd31c6608d4bd502e9267a8fc18251af4
Andrzej <kinopartyclub@gmail.com>
2026-08-19 22:29:13 +0300

    Modify footer text and add share button
    Updated footer text link and added a new button for sharing.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CHANNEL_ID = "@TIR_PegasAT"
 ADMINS = [551563550]
 WATERMARK_TEXT = "MOST AUTO"
 
-FOOTER_TEXT = """<a href="https://samosval.pegasat.com.ua/%d0%be%d0%bd%d0%bb%d0%b0%d0%b9%d0%bd-%d1%81%d0%b5%d1%80%d0%b2%d1%96%d1%81-%d0%bf%d1%96%d0%b4%d0%b1%d0%be%d1%80%d1%83-%d0%b7%d0%b0%d0%bf%d1%87%d0%b0%d1%81%d1%82%d0%b8%d0%bd/"><b>🔗 Більше оригінальних запчастин</b></a>
+FOOTER_TEXT = """<a href="https://pegasat.com.ua"><b>🔗 Більше оригінальних запчастин</b></a>
 
 <b>📞 Потрібна допомога з підбором?</b>
 Наші фахівці швидко підберуть необхідну деталь саме для вашого автомобіля.
@@ -61,8 +61,10 @@ def handle_photo_post(message):
         processed_image = process_image(img_io)
         
         markup = telebot.types.InlineKeyboardMarkup()
-        btn = telebot.types.InlineKeyboardButton(text="Дізнатися ціну / Наявність", url="https://t.me/TIR_PegasAT")
-        markup.add(btn)
+        btn1 = telebot.types.InlineKeyboardButton(text="Дізнатися ціну / Наявність", url="https://t.me/TIR_PegasAT")
+        btn2 = telebot.types.InlineKeyboardButton(text="Відправити другу ↗️", url="https://t.me/share/url?url=https://t.me/TIR_PegasAT")
+        markup.add(btn1)
+        markup.add(btn2)
         
         full_post_text = f"{caption}\n\n{FOOTER_TEXT}"
         
